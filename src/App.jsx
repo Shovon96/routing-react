feb501fbc6afdb63744ec81010efdaf0614a23f0
Shovon96 <fakhruddinshovon@gmail.com>
2023-09-18 02:43:31 +0600

    navbar is done with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <h1 className='text-5xl text-center text-cyan-600 font-bold '>React Routing</h1>
-      
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { TfiMenu, TfiClose } from 'react-icons/Tfi';
+
+
+const Navbar = () => {
+
+    const [navbarOpen, setNabvarOpen] = useState(false)
+
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '/blog', name: 'Blog' }
+    ];
+
+
+    return (
+        <nav className='p-2 md:p-0'>
+            <div className='text-2xl md:hidden' onClick={() => setNabvarOpen(!navbarOpen)}>
+                {
+                    navbarOpen === true ? <TfiClose></TfiClose> : <TfiMenu></TfiMenu>
+                }
+            </div>
+            <ul className={`md:flex justify-center gap-10 my-4 absolute md:static duration-1000 ml-10 bg-fuchsia-400 bg-opacity-50 m-3 px-24 md:px-0 md:my-0 rounded-lg
+                ${navbarOpen ? 'top-2' : '-top-64'}`}>
+                {
+                    routes.map(route =>
+                        <li className="list-none text-xl font-bold text-gray-800 my-2 hover:bg-gray-400 rounded-lg p-1 md:p-2" key={route.id}>
+                            <a href={route.path}>{route.name}</a>
+                        </li>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
